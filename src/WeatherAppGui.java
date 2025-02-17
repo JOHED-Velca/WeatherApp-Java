fb8a6ab76eefc0ed2bdce64ccf303ade2b8b40dd
Jonathan Velasquez <johedvelca@gmail.com>
2025-02-17 15:38:47 -0500

    Weather app created, GUI at 80%, API communication stablished, current date and time being retrieved. GUI not connected yet/

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,112 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui(){
+        //set up our GUI and add  a title
+        super("Weather App");
+
+        //End the program once it has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //set the size of our gui (in pixels)
+        setSize(450, 650);
+
+        //load our GUI at the center of the screen
+        setLocationRelativeTo(null);
+
+        //make our layout manager null to manually position our components within the GUI
+        setLayout(null);
+
+        //prevent GUI resizing
+        setResizable(false);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        //search field
+        JTextField searchTextField = new JTextField();
+
+        //location and size of the component
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        //font style and size
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
+
+        //search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        //change the cursor to a hand cursor when hovering over the button
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        //weather img
+        JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        //temperature text
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+
+        //center the text
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        //weather condition description
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0, 405, 450, 36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
+        //humidity img
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+        add(humidityImage);
+
+        //humidity text
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityText);
+
+        //wind speed img
+        JLabel windspeedImage = new JLabel(loadImage("src/assets/windspeed.png"));
+        windspeedImage.setBounds(220, 500, 74, 66);
+        add(windspeedImage);
+
+        //wind speed text
+        JLabel windspeedText = new JLabel("<html><b>Windspeed</b> 15km/h</html>");
+        windspeedText.setBounds(310, 500, 85, 55);
+        windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(windspeedText);
+
+
+    }
+
+    //creates images in the GUI components
+    private ImageIcon loadImage(String resourcePath){
+        try {
+            //read the image from the path given
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            //returns an image icon so that the component can render it
+            return new ImageIcon(image);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Could not find resource");
+        return null;
+    }
+}
